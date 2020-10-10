17f6dca8993a348d0d09948960f3c4d1f72eba31
andreymal <andriyano-31@mail.ru>
2020-10-10 14:59:00 +0300

    pixel_battle: Update extra 2020.json

--- pixel_battle/env.sh
+++ pixel_battle/env.sh
@@ -6,9 +6,9 @@ mkdir -p my/src
 chmod 700 my
 
 PYTHON="python3"
-BEGIN="2020-10-10_12/2020-10-10_12-01-40.png"
-END="2020-10-10_12/2020-10-10_12-50-00.png"
+BEGIN="2020-10-10_12/2020-10-10_12-01-30.png"
+END="2020-10-10_14/2020-10-10_14-48-20.png"
 EXTRA="2020.json"
 
-# Update for your system \
+# Update for your system
 PB_FONT="/usr/share/fonts/TTF/Arial.TTF"
