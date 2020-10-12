33f9c7c80efc29b29fb0e14116eb641f53e45ccc
andreymal <andriyano-31@mail.ru>
2020-10-13 00:50:54 +0300

    pixel_battle: VK admins are still idiots

--- pixel_battle/env.sh
+++ pixel_battle/env.sh
@@ -7,7 +7,7 @@ chmod 700 my
 
 PYTHON="python3"
 BEGIN="2020-10-10_12/2020-10-10_12-01-30.png"
-END="2020-10-10_14/2020-10-10_14-48-20.png"
+END="2020-10-13_00/2020-10-13_00-40-00.png"
 EXTRA="2020.json"
 
 # Update for your system
